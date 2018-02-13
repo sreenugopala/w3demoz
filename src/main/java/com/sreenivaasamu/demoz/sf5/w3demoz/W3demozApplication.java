a786b63e46e54283cc9237df50cc17cfe4a70a46
Sreenu Gopala <sreenugopala007@outlook.com>
2018-02-13 22:04:40 +0530

    Baseline commit

---
+++ src/main/java/com/sreenivaasamu/demoz/sf5/w3demoz/W3demozApplication.java
@@ -0,0 +1,12 @@
+package com.sreenivaasamu.demoz.sf5.w3demoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3demozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W3demozApplication.class, args);
+	}
+}
